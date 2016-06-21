d13ccc27ef5c9ef1838252e90bd0b9f32edf866b
Gustavo Henrique <gustavohms22@gmail.com>
2016-06-21 20:53:48 -0300

    Atualizacao email

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,16 +1,13 @@
 <?php
-error_reporting(0);
 
 $emaildestinatario = 'gustavohms22@gmail.com';
 $assunto = $_POST['header'];
 $mensagemHTML  = $_POST['msg'];
-$emailsender = 'gusta713@@ gustavodeveloper.com.br  ';
+$emailsender = 'gusta713@@ gustavodeveloper.com.br';
 
-$headers = $_POST['email'] . "\r\n" .
-    'Reply-To:' . $_POST['email'] . "\r\n" .
+$headers = $_POST['email'] . "\n" .
+    'Reply-To:' . $_POST['email'] . "\n" .
     'X-Mailer: PHP/' . phpversion();
-
-
 
 if(!mail($emaildestinatario, $assunto, $mensagemHTML, $headers ,"-r".$emailsender)){ // Se for Postfix
 	echo "<span >Email enviado com sucesso</div>";
